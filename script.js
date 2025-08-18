dcfda3bac8af40cc2fcb91674a04b963fe087bfe
Aniketh Roy Chowdhury <95467658+anideva@users.noreply.github.com>
2025-08-18 16:29:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+
+
+// elements lena 
+const modal= document.querySelector(".modal");
+const openBtn= document.getElementById("openModal"); //yeh ho gaya oppen modal(button) 
+const closeBtn= document.getElementById("closeModal"); //yeh ho gaya close modal(button) 
+
+openBtn.addEventListener("click", () => {
+  modal.classList.add("show"); // CSS se dikh jaayega
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("show"); // chhup jaayega
+});
+
+//ab agar bahar kahi peh bhi cl bhi click karu toh bhi wo close kar dega
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.classList.remove("show");
+  }
+});
+//id => unique element
+//class => list of elements
+
+// 1. button => onclick (open modal) => modal opens, slide top to mid section, background => blurr
+//div => (full screen ) => div => heading, content, button (modal)
+// except modal=> click, modal => close

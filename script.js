224a3d18014268793e98326602b3b257f524048c
Aniketh Roy Chowdhury <95467658+anideva@users.noreply.github.com>
2025-08-19 21:38:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-//your JS code here. If required.
-
-
-// elements lena 
 const modal = document.getElementById("modal");
 const openBtn = document.getElementById("openModal");
 const closeBtn = document.getElementById("closeModal");
 
-// open modal
-openBtn.onclick = () => modal.style.display = "flex";
+openBtn.addEventListener("click", () => {
+	modal.classList.add("show");
+});
 
-// close modal (X button)
-closeBtn.onclick = () => modal.style.display = "none";
+closeBtn.addEventListener("click", () => {
+	modal.classList.remove("show");
+});
 
-// close modal (outside click)
-window.onclick = (e) => {
-  if (e.target === modal) modal.style.display = "none";
-};
-//id => unique element
-//class => list of elements
-
-// 1. button => onclick (open modal) => modal opens, slide top to mid section, background => blurr
-//div => (full screen ) => div => heading, content, button (modal)
-// except modal=> click, modal => close
+window.addEventListener("click", (e) => {
+	if (e.target === modal) {
+		modal.classList.remove("show");
+	}
+});
